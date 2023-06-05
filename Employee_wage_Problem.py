2682ea018c2af6fef2d4a741bb3933514794ef4c
shubham shirke <shirkeshubham84@gmail.com>
2023-06-05 19:40:32 +0530

    [shubham] Add: Employee wage program to Compute monthly wage of full time or part time employee

--- Employee_wage_Problem.py
+++ Employee_wage_Problem.py
@@ -3,85 +3,74 @@
 @Author: shubham shirke
 @Date: 2023-06-05 12:32:30
 @Last Modified by: shubham shirke
-@Last Modified time: 2023-06-05 15:15:30
-@Title : Employee Wage Problem to calculate monthly employee wage.
+@Last Modified time: 2023-06-05 17:01:30
+@Title : Employee Wage Problem to calculate employee wage till 20 working days or 100 working hours.
 '''
 
 import random
 
-def check_attendance():
+wage_per_hour = 20
+full_day_hour = 8
+part_day_hour = 4
+max_monthly_days = 20
+max_working_hour = 100
+
+def generate_random_number():
     """
-    Description : This function generates a random number 0 or 1 using randint function and print employee is present or not.
-    Parameters : none
-    result : Print Employee is present or Absent
+    Description : This function generates a random number 0 or 1.
+    result : random number with value 0 or 1.
     """
-    random_attendance_number = random.randint(0,2)   # generates a random number 0 or 1
-    if random_attendance_number == 1:
-        print("Employee is Present")
+    random_number = random.randint(0,2)
+    return random_number
+
+def calculate_employee_wage():
+    """
+    Description: This function computes employee wage for a month or max working hour for full time or part time employee.
+    parameters : none
+    result : monthly wage for part time or full time employee.
+    """
+    month_days = 0
+    total_working_hour = 0
+    daily_wage = 0
+    month_wage = 0
+    random_number_employee_type = generate_random_number()   #random number to choose full time or part time employee
+    if(random_number_employee_type == 1):
+        while (month_days != max_monthly_days or total_working_hour != max_working_hour):
+            random_number_employee_attendance = generate_random_number() #random number for employee present or absent
+            if(random_number_employee_attendance == 1):
+                print("Full time Employee is Present")
+                daily_wage = full_day_hour * wage_per_hour
+                month_days +=1
+                total_working_hour += full_day_hour
+                month_wage +=daily_wage
+                print(f"Daily wage of full time employee is:{daily_wage}$")
+            else:
+                print("Full time Employee is Absent")
+                print(f"Daily wage of full time employee is:0$")
+            if(month_days == max_monthly_days or total_working_hour == max_working_hour):
+                break
+        print(f"Monthly wage of full time employee is :{month_wage}$")
     else:
-        print("Employee is Absent")
-    return random_attendance_number
-
-def calculate_daily_employee_wage_full_time(wage_per_hour,full_day_hour):
-    """
-    Description : This function calcultes the daily wage of full time employee if he is present having wage per hour rate and full day hour of working.
-                  for a month (20 days)
-    Paramenters : wage_per_hour  - wage per hour for employee.
-                  full_day_hour - working hours for full time employee.
-    result : daily_wage - displays calculted daily wage if full time employee is present or absent.
-    """
-    daily_wage_full_time = 0
-    monthly_wage_full_time = 0
-    for _ in range(20):
-        random_number=check_attendance()   # running check_attendance() 20 times and storing each number in varaible
-        if random_number == 1:
-            daily_wage_full_time = wage_per_hour * full_day_hour     # calcultes daily_wage
-            monthly_wage_full_time += daily_wage_full_time
-            print(f"Daily wage of the Full time employee is: {daily_wage_full_time}$")
-        else:
-            print(f"Daily wage of the Full time employee is:0$")
-    print(f"Monthly wage of full time employee is : {monthly_wage_full_time}$")
-
-
-def calculate_daily_employee_wage_part_time(wage_per_hour,part_day_hour):
-    """
-    Description: This function calculates the daily wahe for part time employee whose working hours are 4 for a month(20 days)
-                 
-    Parameters: wage_per_hour  - wage per hour for employee.
-                part_day_hour - working hours for part time employee.
-    result : Daily_wage_part_time for part time employee.
-    """
-    daily_wage_part_time = 0
-    monthly_wage_part_time=0
-    for _ in range(20):
-        random_number=check_attendance()    # running check_attendance() 20 times and storing each number in varaible
-        if random_number == 1:
-            daily_wage_part_time = wage_per_hour * part_day_hour     # calcultes daily_wage for part time employee
-            monthly_wage_part_time += daily_wage_part_time
-            print(f"Daily wage of the Part time employee is: {daily_wage_part_time}$")
-        else:
-            print(f"Daily wage of the Part time employee is:0$")
-    print(f"Monthly wage of part time employee is:{monthly_wage_part_time}$")
-
+        while (month_days != max_monthly_days or total_working_hour != max_working_hour):
+            random_number_employee_attendance = generate_random_number() #random number for employee present or absent
+            if(random_number_employee_attendance == 1 ):
+                print("Part time Employee is Present")
+                daily_wage = part_day_hour * wage_per_hour
+                month_days +=1
+                total_working_hour += full_day_hour
+                month_wage +=daily_wage
+                print(f"Daily wage of part time employee is:{daily_wage}$")
+            else:
+                print("Employee Absent")
+                print(f"Daily wage of part time employee is:0$")
+            if(month_days == max_monthly_days or total_working_hour == max_working_hour):
+                break
+        print(f"Monthly wage of part time employee is :{month_wage}$")
 
 
 # main function
 def main():
-    wage_per_hour=20
-    full_day_hour=8
-    part_day_hour=4
-    bool = True
-    while(bool):
-        choice = int(input("Enter \n 1: Full time Employee \n 2: Part time Employee: \n 3: Exit \n Enter Choice: "))
-        if choice == 1:
-            calculate_daily_employee_wage_full_time(wage_per_hour,full_day_hour)
-        elif choice == 2 :
-            calculate_daily_employee_wage_part_time(wage_per_hour,part_day_hour)
-        elif choice == 3:
-            bool = False
-        else:
-            print("entered option is incorrect. Please enter 1 for full time employee or 2 for part time employee")
-
+    calculate_employee_wage()
 
 # start Execution
 if __name__ == "__main__":
