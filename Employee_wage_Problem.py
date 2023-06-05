ea3fa825e2a9a9e2b3c95ced45ccd206970e815b
shubham shirke <shirkeshubham84@gmail.com>
2023-06-05 15:41:17 +0530

    [shubham] Add: python employee wage program to compute employee wage for a month

--- Employee_wage_Problem.py
+++ Employee_wage_Problem.py
@@ -20,37 +20,50 @@ def check_attendance():
         print("Employee is Present")
     else:
         print("Employee is Absent")
-    
     return random_attendance_number
 
-def calculate_daily_employee_wage_full_time(random_attendance_number,wage_per_hour,full_day_hour):
+def calculate_daily_employee_wage_full_time(wage_per_hour,full_day_hour):
     """
     Description : This function calcultes the daily wage of full time employee if he is present having wage per hour rate and full day hour of working.
-    Paramenters : random_attendance_number  -  This number is returned from check_attendace() to check is employee is present or not.
+                  for a month (20 days)
+    Paramenters : wage_per_hour  - wage per hour for employee.
+                  full_day_hour - working hours for full time employee.
     result : daily_wage - displays calculted daily wage if full time employee is present or absent.
     """
     daily_wage_full_time = 0
-    if random_attendance_number == 1:
-        daily_wage_full_time = wage_per_hour * full_day_hour     # calcultes daily_wage
-        print(f"Daily wage of the Full time employee is: {daily_wage_full_time} $")
-    else:
-        print(f"Daily wage of the Full time employee is:{daily_wage_full_time}$")
+    monthly_wage_full_time = 0
+    for _ in range(20):
+        random_number=check_attendance()   # running check_attendance() 20 times and storing each number in varaible
+        if random_number == 1:
+            daily_wage_full_time = wage_per_hour * full_day_hour     # calcultes daily_wage
+            monthly_wage_full_time += daily_wage_full_time
+            print(f"Daily wage of the Full time employee is: {daily_wage_full_time}$")
+        else:
+            print(f"Daily wage of the Full time employee is:0$")
+    print(f"Monthly wage of full time employee is : {monthly_wage_full_time}$")
 
 
-def calculate_daily_employee_wage_part_time(random_attendance_number,wage_per_hour,part_day_hour):
+def calculate_daily_employee_wage_part_time(wage_per_hour,part_day_hour):
     """
-    Description: This function calculates the daily wahe for part time employee whose working hours are 4.
-    Parameters: random_attendace_number - contains value 0 to 1 from check_attendace().
-                wage_per_hour  - wage per hour for employee.
+    Description: This function calculates the daily wahe for part time employee whose working hours are 4 for a month(20 days)
+                 
+    Parameters: wage_per_hour  - wage per hour for employee.
                 part_day_hour - working hours for part time employee.
     result : Daily_wage_part_time for part time employee.
     """
     daily_wage_part_time = 0
-    if random_attendance_number == 1:
-        daily_wage_part_time = wage_per_hour * part_day_hour     # calcultes daily_wage for part time employee
-        print(f"Daily wage of the Part time employee is: {daily_wage_part_time} $")
-    else:
-        print(f"Daily wage of the Part time employee is:{daily_wage_part_time}$")
+    monthly_wage_part_time=0
+    for _ in range(20):
+        random_number=check_attendance()    # running check_attendance() 20 times and storing each number in varaible
+        if random_number == 1:
+            daily_wage_part_time = wage_per_hour * part_day_hour     # calcultes daily_wage for part time employee
+            monthly_wage_part_time += daily_wage_part_time
+            print(f"Daily wage of the Part time employee is: {daily_wage_part_time}$")
+        else:
+            print(f"Daily wage of the Part time employee is:0$")
+    print(f"Monthly wage of part time employee is:{monthly_wage_part_time}$")
+
+
 
 # main function
 def main():
@@ -61,11 +74,9 @@ def main():
     while(bool):
         choice = int(input("Enter \n 1: Full time Employee \n 2: Part time Employee: \n 3: Exit \n Enter Choice: "))
         if choice == 1:
-            random_attendace_number=check_attendance()
-            calculate_daily_employee_wage_full_time(random_attendace_number,wage_per_hour,full_day_hour)
+            calculate_daily_employee_wage_full_time(wage_per_hour,full_day_hour)
         elif choice == 2 :
-            random_attendace_number=check_attendance()
-            calculate_daily_employee_wage_part_time(random_attendace_number,wage_per_hour,part_day_hour)
+            calculate_daily_employee_wage_part_time(wage_per_hour,part_day_hour)
         elif choice == 3:
             bool = False
         else:
