9a6c89d2c77a93e02a14b9291a7a49b75a0aba52
shubham shirke <shirkeshubham84@gmail.com>
2023-06-06 15:27:58 +0530

    [shubham] Add: store the day and daily wage of employee in a key value format and display.

--- Employee_wage_Problem.py
+++ Employee_wage_Problem.py
@@ -3,7 +3,7 @@
 @Date: 2023-06-05 12:32:30
 @Last Modified by: shubham shirke
 @Last Modified time: 2023-06-05 13:48:30
-@Title : Employee Wage Problem to get list of daily wages for a month or max working hours.
+@Title : Employee Wage Problem to get dictionary of daily wages for a month or max working hours having key as day number and value as daily wage.
 '''
 
 import random
@@ -16,7 +16,7 @@ max_working_hour = 100
 is_full_time = 1
 is_part_time = 2
 is_absent = 0
-daily_wage_list=[]
+daily_wage_list=[]   # initializing empty list
 
 def generate_random_number():
     """
@@ -88,13 +88,31 @@ def calculate_employee_wage():
     
     # print monthly wage and total working hour
     print()
+    
     print(f"Monthly wage of Employee is: {month_wage}$")
-    print(f"Total working time of Employee is: {total_work_hour}")
-    print(f"The list of daily wages for the month are:{daily_wage_list}")
+    print(f"Total working time of Employee is: {total_work_hour} hours")
+    
+    # retrun list of daily wages
+    return daily_wage_list
+
+def get_daily_wage_dict(daily_wage_list):
+    """
+    Description : This function generates a dictionary from list of daily wages
+    Parameters  : daily_wage_list -- this values is returned from calculate_employee_wage() containing values of daily wages.
+    result : daily_wage_dict 
+    """
+    daily_wage_dict = {}    # initializing empty dictionary
+
+    for day, value in enumerate(daily_wage_list, start=1):
+        daily_wage_dict[day] = value
+
+    print(f"The list of daily wages for the month are: {daily_wage_dict}")
+
 
 # main
 def main():
-    calculate_employee_wage()
+    daily_wages_list=calculate_employee_wage()
+    get_daily_wage_dict(daily_wages_list)
 
 
 if __name__ == "__main__":
